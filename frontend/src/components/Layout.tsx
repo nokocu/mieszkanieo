c7755cbbb53edae0999be9d4176df8fca97de2a9
nokocu <nokocu@gmail.com>
2025-07-31 19:53:54 +0200

    minor: navbar styling

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { App as AppSidebar } from './Sidebar'
 import { SidebarProvider, SidebarInset, SidebarTrigger } from './ui/sidebar'
-import { Separator } from './ui/separator'
 import { Navbar } from './Navbar'
 import { Toaster } from './ui/sonner'
 import Properties from './Properties'
@@ -13,7 +12,6 @@ export function Layout() {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
           <Navbar />
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
